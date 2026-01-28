6e25c70c556f8b643c911fbcb30483396cf11e2f
Wu, Yutong <yutong.wu@amd.com>
2026-01-28 09:53:24 +0000

    can run ll

--- deep_ep/__init__.py
+++ deep_ep/__init__.py
@@ -1,7 +1,31 @@
+import os
 import torch
 
 from .utils import EventOverlap
-from .buffer import Buffer
 
-# noinspection PyUnresolvedReferences
-from deep_ep_cpp import Config, topk_idx_t
+def _detect_backend():
+    backend_env = os.environ.get('DEEP_EP_BACKEND', '').lower()
+    if backend_env in ('cuda', 'rocm'):
+        return backend_env
+    
+    if hasattr(torch.version, 'hip') and torch.version.hip is not None:
+        return 'rocm'
+    
+    if os.environ.get('ROCM_PATH') or os.environ.get('HIP_PATH'):
+        return 'rocm'
+    
+    return 'cuda'
+
+
+_backend = _detect_backend()
+
+if _backend == 'rocm':
+    from .buffer_rocm import BufferROCm as Buffer, Config
+    from mori.cpp import topk_idx_t 
+else:
+    from .buffer import Buffer
+    # noinspection PyUnresolvedReferences
+    from deep_ep_cpp import Config, topk_idx_t
+
+__all__ = ['Buffer', 'EventOverlap', 'Config', 'EventHandle', 'topk_idx_t']
+
